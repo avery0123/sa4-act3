5cf114feb23f5b0e7b1428d55be3f555d001d9b0
Avery Oliver <averyaoliver03@gmail.com>
2024-02-07 21:10:30 -0600

    new edit

--- guess_number.py
+++ guess_number.py
@@ -2,13 +2,20 @@ number = 10
 print("I'm thinking of a number...")
 
 while True:
-    guess = input("What number am I thinking of? (Enter 'q' to quit) ")
+    guess_str = input("What number am I thinking of? (Enter 'q' to quit) ")
 
-    if guess.lower() == 'q':
+    if guess_str.lower() == 'q':
         print(f"The number was {number}. Goodbye!")
         break
-    elif int(guess) == number:
-        print("Congratulations! You guessed the right number.")
-        break
     else:
-        print("Sorry! Try again.")
+        try:
+            guess = int(guess_str)
+            if guess == number:
+                print("Congratulations! You guessed the right number.")
+                break
+            elif guess < number:
+                print("Sorry! Your guess is too low. Try again.")
+            else:
+                print("Sorry! Your guess is too high. Try again.")
+        except ValueError:
+            print("Invalid input. Please enter a number or 'q' to quit.")
