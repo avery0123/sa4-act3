6b53fb8ce096c1bd4de09f5b4fdc227c05723591
Avery Oliver <averyaoliver03@gmail.com>
2024-02-07 21:25:43 -0600

    modified

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,25 @@
 number = 10
+max_attempts = 3  # Set the maximum number of guesses
 print("I'm thinking of a number...")
-
-while True:
-    guess = input("What number am I thinking of? (Enter 'q' to quit) ")
-
-    if guess.lower() == 'q':
+attempts = 0
+while attempts < max_attempts:
+    guess_str = input(f"What number am I thinking of? (Enter 'q' to quit) You have {max_attempts - attempts} guesses left: ")
+    if guess_str.lower() == 'q':
         print(f"The number was {number}. Goodbye!")
         break
-    elif int(guess) == number:
-        print("Congratulations! You guessed the right number.")
-        break
     else:
-        print("Sorry! Try again.")
+        try:
+            guess = int(guess_str)
+            attempts += 1
+
+            if guess == number:
+                print("Congratulations! You guessed the right number.")
+                break
+            else:
+                if attempts < max_attempts:
+                    print(f"Sorry! Your guess is incorrect. You have {max_attempts - attempts} guesses left. Try again.")
+                else:
+                    print(f"Sorry! Your guess is incorrect. The number was {number}. You're out of guesses. Goodbye!")
+                    break
+        except ValueError:
+            print("Invalid input. Please enter a number or 'q' to quit.")
